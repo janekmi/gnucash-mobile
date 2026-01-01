bda66df3df137bc260a7c6d557ae4baf0bf995c0
Jan Michalski <jan.michalski@pm.me>
2026-01-01 21:22:53 +0000

    android: gradle updates (WIP)
    Signed-off-by: Jan Michalski <jan.michalski@pm.me>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.xxx"
+    namespace = "com.nicktylah.gnucash_mobile2"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.xxx"
+        applicationId = "com.nicktylah.gnucash_mobile2"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
